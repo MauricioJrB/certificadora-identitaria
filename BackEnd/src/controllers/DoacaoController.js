fcdab89b03a319801e57e6c3e5961ff59cffe5d2
Leandro Henrique <leandrohenriquetti@gmail.com>
2024-11-10 18:07:00 -0300

    Helpers para o banco/Controller para docao e rota doacao adicionado. Testes e finalização serão feitos.

---
+++ BackEnd/src/controllers/DoacaoController.js
@@ -0,0 +1,31 @@
+import productModel from '../models/Product'
+const validator = require("validator");
+
+class DoacaoController {
+    static DonationPrduct = async (req, res) => {
+        const {name, description} = req.body;
+        
+        if(!validator.isLength(name, {min: 5, max: 20})) {
+            console.log("Tamanho do nome do produto insuficiente!");
+        }
+        if(!validator.isAlpha(name) && !validator.isAlpha(description)) {
+            console.log("O nome/descrição do produto deve ser apenas uma string!");
+        }
+
+        try{
+            const newProduct = new productModel({
+                name: name,
+                description: description
+            })
+
+            const savedProduct = await newProduct.save();
+
+            return res.status(200).json(savedProduct);
+
+        } catch(error){
+            return res.status(500).json({msg: "An unexpected error occured", error: error.message})
+        }
+    }
+}
+
+export default DoacaoController;
