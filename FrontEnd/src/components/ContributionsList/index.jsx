8853d96201f8c04571d50de3471eb63f20779895
Victor Moreira <zvihugo@hotmail.com>
2024-11-11 10:39:06 -0300

    refactor: visualização doações
    Adicionado mais 2 div para melhorar a estilização e fazer com que os compontes fiquem dispostos conforme o figma do projeto

--- FrontEnd/src/components/ContributionsList/index.jsx
+++ FrontEnd/src/components/ContributionsList/index.jsx
@@ -5,9 +5,15 @@ export const ContributionsList = ({ contributions }) => (
   <div className={styles.contributionsContainer}>
     {contributions.map((contrib, index) => (
       <div key={index} className={styles.contributionItem}>
-        <h3 className={styles.contributionName}>{contrib.name}</h3>
-        <p className={styles.contributionDescription}>{contrib.contribution}</p>
-        <p className={styles.contributionTime}>{contrib.timeAgo}</p>
+        <div className={styles.contributionRigth}>
+          <h3 className={styles.contributionName}>{contrib.name}</h3>
+          <p className={styles.contributionDescription}>{contrib.contribution}</p>
+        </div>
+        <div className={styles.contributionLeft}>
+          
+          <p className={styles.contributionTime}>{contrib.timeAgo}</p>
+        </div>
+        <div className={styles.horizontalLine}></div>
       </div>
     ))}
   </div>
