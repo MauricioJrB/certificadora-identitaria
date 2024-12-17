c5547aa6971d1313fd32b783b88e8a5f0dd0b0e9
MauricioJrB <mjunior1205@gmail.com>
2024-12-17 17:18:04 -0300

    feat: add get products on DoacaoController

--- BackEnd/src/controllers/DoacaoController.js
+++ BackEnd/src/controllers/DoacaoController.js
@@ -4,7 +4,7 @@ import expressValidator from 'validator';
 class DoacaoController {
     static DonationPrduct = async (req, res) => {
         const validator = expressValidator;
-        const {name, description} = req.body;
+        const {name, description, date} = req.body;
         
         if(!validator.isLength(name, {min: 5, max: 20})) {
             console.log("Tamanho do nome do produto insuficiente!");
@@ -12,11 +12,14 @@ class DoacaoController {
         if(!validator.isAlpha(name) && !validator.isAlpha(description)) {
             console.log("O nome/descrição do produto deve ser apenas uma string!");
         }
-
+        if(!validator.isAlpha(date)) {
+            console.log("A data do produto deve ser apenas uma string!");
+        }
         try{
             const newProduct = new productModel({
                 name: name,
-                description: description
+                description: description,
+                date: date,
             })
 
             const savedProduct = await newProduct.save();
@@ -24,7 +27,16 @@ class DoacaoController {
             return res.status(200).json(savedProduct);
 
         } catch(error){
-            return res.status(500).json({msg: "An unexpected error occured", error: error.message})
+            return res.status(500).json({msg: "An unexpected error occured", error: error.message});
+        }
+    }
+
+    static getProducts = async (req, res) => {
+        try {
+            const products = await productModel.find().exec();
+            return res.status(200).json(products);
+        } catch (error) {
+            return res.status(500).json({msg: "An unexpected error occured", error: error.message});
         }
     }
 }
