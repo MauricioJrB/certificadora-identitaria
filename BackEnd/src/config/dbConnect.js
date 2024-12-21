5e20e920ffba6aaa155664992f96ea2d76a5a466
MauricioJrB <mjunior1205@gmail.com>
2024-12-21 14:07:39 -0300

    feat: updated dbConnect.js

--- BackEnd/src/config/dbConnect.js
+++ BackEnd/src/config/dbConnect.js
@@ -4,8 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const mongoURI = process.env.MONGODB_URI;
-
-mongoose.connect(mongoURI);
-const db = mongoose.connection;
+mongoose.connect(mongoURI)
+const db = mongoose.connection; 
 
 export default db;
