27dcb1938a3a13b6f948e9750d35e0a3ccc36af4
MauricioJrB <mjunior1205@gmail.com>
2024-12-21 13:56:21 -0300

    feat: updated server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,7 +2,7 @@ import app from "./src/app.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.SERVER_PORT || 8001;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}` );
